9bed6085083a666a3650405859bebbd1d23a81f6
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-06-26 22:01:50 +0200

    .

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const commands = [
   },
   // Valid command: "no response"
   { expression: /no\sresponse/i, handle: () => {} },
-  // Valid command: "go" with optional additional arguments, for example: "go west".
-  { expression: /go\s?(.*)/i, handle: command => "OK, " + command }
+  // Valid command: "go" with additional arguments, for example: "go west".
+  { expression: /go\s(.*)/i, handle: command => "OK, " + command }
 ];
 
 const App = () => <Terminal commands={commands} />;
