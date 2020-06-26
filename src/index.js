8b21a5cc2c4371b62ee1b08f3c9548768c131c63
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-06-26 20:56:54 +0200

    .

--- src/index.js
+++ src/index.js
@@ -7,49 +7,17 @@ const commands = [
   {
     text: "bouwe",
     handle: () =>
-      "That's me! :) I am Bouwe, 42 years old, from Drachten, Netherlands. I am a software engineer.",
-    description: "More about me."
+      "That's me! :) I am Bouwe, 42 years old, from Drachten, Netherlands. I am a software engineer."
   },
   {
     text: "contact",
-    handle: () => "You can contact me on Twitter: @bouwe",
-    description: "Ways to contact me."
-  }
+    handle: () => "You can contact me on Twitter: @bouwe"
+  },
+  { text: "niks", handle: () => {} }
 ];
 
-function getSpaces(howMany) {
-  let spaces = [];
-  for (let index = 0; index < howMany; index++) {
-    spaces.push(<>&nbsp;</>);
-  }
-  return <>{spaces}</>;
-}
-
-function displayAllCommands() {
-  return (
-    <>
-      <br />
-      The following commands are available:
-      <br />
-      <br />
-      {commands.map(command => (
-        <>
-          &nbsp; &nbsp;
-          {command.text}
-          {getSpaces(20 - command.text.length)}
-          {command.description}
-          <br />
-        </>
-      ))}
-      <br />
-    </>
-  );
-}
-
 function handleCommand(text) {
-  if (text === "?" || text === "help") return displayAllCommands();
-
-  const command = commands.find(command => command.text === text);
+  const command = commands.find(command => command.text.toUpperCase() === text.toUpperCase());
   if (!command) return "Invalid command.";
 
   return command.handle();
