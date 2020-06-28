609336eee8410166f8f3d92f18dd7f83637b3f37
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-06-28 16:14:40 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,18 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import Terminal from "./Terminal";
-
-const commands = [
-  // Valid command: "hello"
-  {
-    expression: /hello/i,
-    handle: () => "Hello to you too :)"
-  },
-  // Valid command: "no response"
-  { expression: /no\sresponse/i, handle: () => {} },
-  // Valid command: "go" with additional arguments, for example: "go west".
-  { expression: /go\s(.*)/i, handle: command => "OK, " + command }
-];
+import commands from "./commands";
 
 const App = () => <Terminal commands={commands} />;
 
