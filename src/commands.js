5d7bd3c05f4b78ff2ea167496546d971900bd40e
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-07-23 22:09:08 +0200

    fix: remove debugging

--- src/commands.js
+++ src/commands.js
@@ -3,12 +3,13 @@ const commands = [
   {
     expression: /hello/i,
     handle: () => {
-      console.log('doing something...')
       return 'Hello to you too :)'
     },
   },
+
   // Valid command: "no response"
   { expression: /no\sresponse/i, handle: () => {} },
+
   // Valid command: "go" with additional arguments, for example: "go west".
   { expression: /go\s(.*)/i, handle: (command) => 'OK, ' + command },
 ]
