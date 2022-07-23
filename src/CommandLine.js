bccd9bbe6145243764e2726ab78400b03d6a9a48
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-07-23 23:08:05 +0200

    chore: remove React import

--- src/CommandLine.js
+++ src/CommandLine.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import './CommandLine.module.css'
 
 const CommandLine = ({ handleCommand }) => {
--- src/Terminal.js
+++ src/Terminal.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import CommandLine from './CommandLine'
 import Help from './Help'
 import History from './History'
