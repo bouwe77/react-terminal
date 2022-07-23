58d03ec6890a109fe44185f0155eea5396eb66d2
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-07-23 22:56:20 +0200

    feat: add help command that shows help text for commands that have help texts

--- src/Terminal.js
+++ src/Terminal.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react'
 import CommandLine from './CommandLine'
+import Help from './Help'
 import History from './History'
+
+const defaultHelpCommand = {
+  name: 'help',
+  expression: /help/i,
+  help: {
+    example: 'help',
+    description: 'Show this help',
+  },
+}
 
 const Terminal = ({ commands }) => {
   const [history, setHistory] = useState([])
 
+  const helpCommand = {
+    ...defaultHelpCommand,
+    handle: () => {
+      return <Help commands={commands} />
+    },
+  }
+
+  const allCommands = [helpCommand, ...commands]
+
   const handleCommand = (commandText) => {
-    const command = commands.find((command) => command.expression?.test(commandText))
+    const command = allCommands.find((command) => command.expression?.test(commandText))
     let response
 
     if (!command) response = 'Invalid command'
