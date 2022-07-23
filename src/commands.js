0a00e1405f85e8bc77d8a0a1729ed7e6e3bb37f7
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-07-23 22:56:39 +0200

    fix: add help texts

--- src/commands.js
+++ src/commands.js
@@ -2,16 +2,34 @@ const commands = [
   // Valid command: "hello"
   {
     expression: /hello/i,
+    help: {
+      example: 'hello',
+      description: 'Say hello to the terminal',
+    },
     handle: () => {
       return 'Hello to you too :)'
     },
   },
 
   // Valid command: "no response"
-  { expression: /no\sresponse/i, handle: () => {} },
+  {
+    expression: /no\sresponse/i,
+    help: {
+      example: 'no response',
+      description: 'No response',
+    },
+    handle: () => {},
+  },
 
   // Valid command: "go" with additional arguments, for example: "go west".
-  { expression: /go\s(.*)/i, handle: (command) => 'OK, ' + command },
+  {
+    expression: /go\s(.*)/i,
+    help: {
+      example: 'go [direction]',
+      description: 'Go to the specified [direction]',
+    },
+    handle: (command) => "OK, let's " + command,
+  },
 ]
 
 export default commands
